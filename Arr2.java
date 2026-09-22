63c5e50d1f0ef1f3ebb2244b6a2c2080217eb960
sasivarman-s <sureshraman2680@gmail.com>
2026-09-22 14:10:59 +0530

    done

---
+++ Arr2.java
@@ -0,0 +1,8 @@
+public class Arr2 {
+    public static void main(String[] args) {
+        int[] arrey = {1,2,3,4,5,6,7,8,9,10};
+        int len = arrey.length;
+        System.out.println("by lenght method: "+arrey.length);
+        System.out.println("by saving in variable by also length method: "+len);
+    }
+}
